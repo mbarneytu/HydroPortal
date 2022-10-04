3dff013d1c7b917cf5e102db4d2109ecc7e5e285
mbarneytu <mbarney@tu.org>
2022-10-04 09:09:50 -0600

    Make text inputs wider

--- ui.R
+++ ui.R
@@ -7,32 +7,35 @@ ui <- fluidPage(
   h6("Required fields are marked with *"),
   fluidRow(
     column(width = 6,
-           textInput("site_name*", "*Site Name"), 
-           textInput("contact_name", "*TU Staff Contact Name"),
-           textInput("contact_email", "*TU Staff Contact Email"),
-           dateInput("install_date", "*Date of installation"),
-           textInput("user_site_id", "Site ID"), 
-           textAreaInput("equipment", "Equipment"),
-           textInput("landowner", "Landowner"),
-           textAreaInput("notes", "Notes")
+           textInput("site_name*", "*Site Name", width = "90%"),
+           textInput("contact_name", "*TU Staff Contact Name", width = "90%"),
+           textInput("contact_email", "*TU Staff Contact Email", width = "90%"),
+           dateInput("install_date", "*Date of installation", width = "90%"),
+           textInput("user_site_id", "Site ID", width = "90%"), 
+           textAreaInput("equipment", "Equipment", width = "90%"),
+           textInput("landowner", "Landowner", width = "90%"),
+           textAreaInput("notes", "Notes", width = "90%"),
     ),
     
     column(width = 6,
-           fluidRow(
-             column(width = 12,
-                    leafletOutput("map")
-             )
-           ),
-           br(),
-           fluidRow(
-             column(width = 6,
-                    numericInput("lat", "*Latitude", value = "")),
-             column(width = 6,
-                   numericInput("long", "*Longitude", value = "")
-                   )
-           )
+       fluidRow(
+         column(width = 12,
+                leafletOutput("map")
+         )
+       ),
+       br(),
+       fluidRow(
+         column(width = 6,
+                numericInput("lat", "*Latitude", value = "")),
+         column(width = 6,
+               numericInput("long", "*Longitude", value = "")
+               )
+       )
     )
   ),
+  
+  br(),
+  
   fluidRow(
     column(width = 2, offset = 5,
            actionButton("btnSave", "Save Site", width = "100%", class = "btn-success")
