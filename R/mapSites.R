92e9c6260a17802be1ea382a445f2a0e5d247b72
mbarneytu <mbarney@tu.org>
2022-10-26 17:28:09 -0600

    Send id of the clicked marker back to server.

--- R/mapSites.R
+++ R/mapSites.R
@@ -4,17 +4,28 @@ library(DBI)
 
 mapSitesUI <- function(id) {
   tagList(
-    titlePanel("View Sites"),
-    fluidRow(
-      column(width = 12,
-             leafletOutput(NS(id, "map"))
+    h4("Choose a site from the map:"),
+
+    tabsetPanel(
+      id = NS(id,"switcher"),
+      type = "hidden",
+      
+      tabPanelBody(
+        value = "mapView",
+        leafletOutput(NS(id, "map"))
+      ),
+      
+      tabPanelBody(
+        value = "dataView",
+        textOutput(NS(id, "data")),
+        actionButton(NS(id, "btnReturnToMap"), "Return to Map")
       )
     )
   )
 }
 
 selectSites <- function() {
-  query <- "CALL sel_site_locations"
+  query <- "CALL sel_site_locations" # returns lat, lon, site_name, site_id
   res <- dbGetQuery(pool, query)
 }
 
@@ -22,26 +33,42 @@ mapSitesServer <- function(id) {
   moduleServer(id, function(input, output, session) {
     
     # Load all sites into a dataframe
-    df <- selectSites() 
+    df <- selectSites()
+    
+    viewDataLink <- actionLink(
+      inputId = "foo", # This Id isn't used in this case.
+      label = "View Data",
+      # Here we use Shiny.setInputValue() to set up a reactive input.
+      # (see: shiny.rstudio.com/articles/communicating-with-js.html)
+      # Note that the first argument must be preceded by our module's id.
+      onclick = 'Shiny.setInputValue(\"map1-link\", this.id, {priority: "event"})'
+    )
     
     output$map <- renderLeaflet({
-      leaflet(data = df) %>% 
-        
+      leaflet(data = df) %>%
         addProviderTiles(providers$Esri.WorldTopoMap, group = "Topo") %>%
         addProviderTiles(providers$Esri.WorldImagery, group = "Satellite") %>%
         
         addLayersControl(baseGroups = c("Topo", "Satellite"),
                          options = layersControlOptions(collapsed = FALSE)) %>% 
         
-        fitBounds(-125.1, 49, -67.1, 25.2) %>% # zoom to Lower 48 states 
+        # zoom to Lower 48 states 
+        fitBounds(-125.1, 49, -67.1, 25.2) %>% 
         
-        addMarkers(lng = ~lon, lat = ~lat, popup = ~as.character(site_name), 
-                   label = ~as.character(site_name), layerId = ~site_id)
+        addMarkers(
+          lng = df$lon, lat = df$lat,
+          label = df$site_name,
+          layerId = df$site_id,
+          popup = paste(
+            "<b>", df$site_name, "</b></br>",
+            viewDataLink
+          )
+        )
     })
-
-    observeEvent(input$map_marker_click, {
-      # Handle marker clicks
-      message(paste0("id=", input$map_marker_click$id))
+    observeEvent(input$link, {
+      myPoint <- reactiveVal(input$map_marker_click)
+      
+      print(myPoint()$id)
     })
   })
 }
