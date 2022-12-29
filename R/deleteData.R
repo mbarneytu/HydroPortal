61ddeaddf97d996fb7a24f3ff45ca75855a490f7
mbarneytu <mbarney@tu.org>
2022-12-29 08:11:17 -0700

    basic myTable reactive works. Ready to add delete logic back in

--- R/deleteData.R
+++ R/deleteData.R
@@ -17,46 +17,54 @@ deleteDataUI <- function(id) {
 
 deleteDataServer <- function(id, selectedSiteId) {
   moduleServer(id, function(input, output, session) {
-    
-    myTable <- eventReactive(selectedSiteId, {
-      loadUploads(selectedSiteId())
-    })
-    
+    myTable <- reactive(loadUploads(selectedSiteId()))
+
     output$table <- renderDT({
-      req(myTable())
       datatable(myTable() |> select(!file_upload_id), 
                 selection = "single")
     })
-    
-    observeEvent(input$btnDelete, {
-      index = input$table_rows_selected
-      selectedRow <- myTable()[index,]
-      if (length(index)) {
-        
-        ns <- session$ns
-        modal_confirm <- modalDialog(
-          paste0("Are you sure you want to delete ", 
-                 selectedRow$row_count, " rows, including data between ",
-                 selectedRow$obs_min_datetime |> as_date(), " and ",
-                 selectedRow$obs_max_datetime |> as_date(),
-                 "?"),
-          title = "Deleting datapoints",
-          footer = tagList(
-            actionButton(ns("btnConfirmDel"), "Delete", class = "btn btn-danger"),
-            actionButton(ns("btnCancel"), "Cancel")
-          )
-        )
-        showModal(modal_confirm)
-        
-        observeEvent(input$btnConfirmDel, {
-          deleteUpload(selectedRow$file_upload_id)
-          removeModal()
-        })
 
-        observeEvent(input$btnCancel, {
-          removeModal()
-        })
-      }
+    observeEvent(input$table_rows_selected,{
+      print(paste0("row # selected: ", input$table_rows_selected,
+                   ". Upload_id: ", myTable()[input$table_rows_selected,]$file_upload_id))
     })
+    
+    selectedRow <- reactive(tibble())
+    # 
+    # observeEvent(input$btnDelete, {
+    #   req(!is.null(input$table_rows_selected))
+    #   index <- input$table_rows_selected
+    #   if (length(index)) {
+    #     selectedRow <<- isolate(myTable()[index,])
+    #     
+    #     # req(selectedRow)
+    #     ns <- session$ns
+    #     modal_confirm <- modalDialog(
+    #       paste0("Are you sure you want to delete ",
+    #              selectedRow$row_count, " rows, including data between ",
+    #              selectedRow$obs_min_datetime |> as_date(), " and ",
+    #              selectedRow$obs_max_datetime |> as_date(),
+    #              "?"),
+    #       title = "Deleting datapoints",
+    #       footer = tagList(
+    #         actionButton(ns("btnConfirmDel"), "Delete", class = "btn btn-danger"),
+    #         actionButton(ns("btnCancel"), "Cancel")
+    #       )
+    #     )
+    #     showModal(modal_confirm)
+    #   }
+    # }, ignoreInit = TRUE)
+    # 
+    # observeEvent(input$btnConfirmDel, {
+    #   print(paste0("would have deleted id# ", isolate(selectedRow$file_upload_id)))
+    #   # deleteUpload(selectedRow$file_upload_id)
+    #   # myTable <<- reactive(loadUploads(selectedSiteId()))
+    #   # selectedRow <<- tibble()
+    #   removeModal()
+    # }, ignoreInit = TRUE)
+    # 
+    # observeEvent(input$btnCancel, {
+    #   removeModal()
+    # })
   })
 }
