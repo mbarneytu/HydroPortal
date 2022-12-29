fcc80681d66cf7d730243cde7b892912bdc9ca2c
mbarneytu <mbarney@tu.org>
2022-12-29 09:08:38 -0700

    Delete works, but DT doesn't update afterward

--- R/deleteData.R
+++ R/deleteData.R
@@ -28,48 +28,39 @@ deleteDataServer <- function(id, selectedSiteId) {
     
     observeEvent(input$table_rows_selected,{
       selectedRow <<- reactive(myTable()[input$table_rows_selected,])
-      print(paste0("row # selected: ", input$table_rows_selected,
-                   ". Upload_id: ", selectedRow()$file_upload_id))
     })
     
 
     observeEvent(input$btnDelete, {
       req(input$table_rows_selected)
-      print(paste0("would have deleted id# ", selectedRow()$file_upload_id))
+      
+      ns <- session$ns
+      modal_confirm <- modalDialog(
+        paste0("Are you sure you want to delete ",
+               selectedRow()$row_count, " rows, including data between ",
+               selectedRow()$obs_min_datetime |> as_date(), " and ",
+               selectedRow()$obs_max_datetime |> as_date(),
+               "?"),
+        title = "Deleting datapoints",
+        footer = tagList(
+          actionButton(ns("btnConfirmDel"), "Delete", class = "btn btn-danger"),
+          actionButton(ns("btnCancel"), "Cancel")
+        )
+      )
+      showModal(modal_confirm)
     })
     
-    #   index <- input$table_rows_selected
-    #   if (length(index)) {
-    #     selectedRow <<- isolate(myTable()[index,])
-    #     
-    #     # req(selectedRow)
-    #     ns <- session$ns
-    #     modal_confirm <- modalDialog(
-    #       paste0("Are you sure you want to delete ",
-    #              selectedRow$row_count, " rows, including data between ",
-    #              selectedRow$obs_min_datetime |> as_date(), " and ",
-    #              selectedRow$obs_max_datetime |> as_date(),
-    #              "?"),
-    #       title = "Deleting datapoints",
-    #       footer = tagList(
-    #         actionButton(ns("btnConfirmDel"), "Delete", class = "btn btn-danger"),
-    #         actionButton(ns("btnCancel"), "Cancel")
-    #       )
-    #     )
-    #     showModal(modal_confirm)
-    #   }
-    # }, ignoreInit = TRUE)
-    # 
-    # observeEvent(input$btnConfirmDel, {
-    #   print(paste0("would have deleted id# ", isolate(selectedRow$file_upload_id)))
-    #   # deleteUpload(selectedRow$file_upload_id)
-    #   # myTable <<- reactive(loadUploads(selectedSiteId()))
-    #   # selectedRow <<- tibble()
-    #   removeModal()
-    # }, ignoreInit = TRUE)
-    # 
-    # observeEvent(input$btnCancel, {
-    #   removeModal()
-    # })
+    observeEvent(input$btnConfirmDel, {
+      print(paste0("Deleting id# ", selectedRow()$file_upload_id))
+      deleteUpload(selectedRow()$file_upload_id)
+      print(paste0("Before re-loading, myTable has rows: ", nrow(myTable())))
+      myTable <<- reactive(loadUploads(selectedSiteId()))
+      print(paste0("After re-loading, myTable has rows: ", nrow(myTable())))
+      removeModal()
+    })
+
+    observeEvent(input$btnCancel, {
+      removeModal()
+    })
   })
 }
